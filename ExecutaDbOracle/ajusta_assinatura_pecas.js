df26faaf9d204db39ffa00bf5c2291eeae73d2ea
Gustavo Santos <gugahh.br@gmail.com>
2025-03-13 13:21:38 -0300

    ajuste assinaturas

--- ExecutaDbOracle/ajusta_assinatura_pecas.js
+++ ExecutaDbOracle/ajusta_assinatura_pecas.js
@@ -89,20 +89,25 @@ async function obtemProcessos(connection, numRegistros) {
 
   var result = await connection.execute(
     `
-        SELECT 
-            DISTINCT(tmpp.MTPP_NR_PROCESSO_CNJ) as CNJ
-        FROM TJRJ_METADADOS_PECAS_PROCESSO tmpp 
-        WHERE 1=1 
-            AND tmpp.MTPP_TTDL_DK = 86
-            -- AND tmpp.MTPP_DT_PESQ_ASSINATURAS IS not NULL
-            AND tmpp.MTPP_DT_INCLUSAO >= to_date('01/01/2024', 'dd/mm/yyyy')
-            AND NOT EXISTS 
-            (
-                SELECT 1
-                FROM TJRJ_METADADOS_PECAS_ASSINAT ASS 
-                WHERE ASS.MSPA_MTPP_DK = tmpp.MTPP_DK 
-            )
-            and ROWNUM <= :pNumRegistros 
+        SELECT CNJ 
+        FROM 
+        (
+            SELECT 
+                DISTINCT(tmpp.MTPP_NR_PROCESSO_CNJ) AS CNJ
+            FROM TJRJ_METADADOS_PECAS_PROCESSO tmpp 
+            WHERE 1=1 
+                AND tmpp.MTPP_TTDL_DK = 86
+                -- AND tmpp.MTPP_DT_PESQ_ASSINATURAS IS not NULL
+                AND tmpp.MTPP_DT_INCLUSAO >= to_date('01/01/2024', 'dd/mm/yyyy')
+                AND NOT EXISTS 
+                (
+                    SELECT 1
+                    FROM TJRJ_METADADOS_PECAS_ASSINAT ASS 
+                    WHERE ASS.MSPA_MTPP_DK = tmpp.MTPP_DK 
+                )
+            ORDER BY 1 ASC 
+        )
+        Where ROWNUM <= :pNumRegistros 
  `,
      {pNumRegistros: numRegistros},  
      {
