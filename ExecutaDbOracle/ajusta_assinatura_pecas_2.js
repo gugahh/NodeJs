f39fe17e6baeba58cab4109dfc636f3b0ad82cf6
Gustavo Santos <gugahh.br@gmail.com>
2026-04-16 15:26:27 -0300

    Sync2

--- ExecutaDbOracle/ajusta_assinatura_pecas_2.js
+++ ExecutaDbOracle/ajusta_assinatura_pecas_2.js
@@ -19,17 +19,18 @@ console.log("\n===== Solicita atualizacao de Assinatura de Pecas do DCP (2) ====
 const urlUpdatePecas = 'http://d-extrair-assinatura-digital-peca-dcp.apps.ocpn.mprj.mp.br/dcp/processar/assinatura/peca/processo-iddocumento/?cnj/?id_documento';
 
 // Producao
-// const urlUpdatePecas = "";
+const urlUpdatePecas = "http://extrair-assinatura-digital-peca-dcp.apps.ocpn.mprj.mp.br/dcp/processar/assinatura/peca/processo-iddocumento/?cnj/?id_documento";
 
 
 // Obtendo definicoes de Banco de Dados a partir da linha de comando.
-if (!Array.isArray(myArgs) || myArgs.length != 5) {
+if (!Array.isArray(myArgs) || myArgs.length != 6) {
   console.error("\n====Erro!====");
   
   console.error("* Devem ser informados os parametros: \n" + 
      "\t- String de Conexao\n" + 
      "\t- usuario \n" +
      "\t- senha \n" +
+     "\t- trimestre \n" +
      "\t- quant de registros a processar \n" +
      "\t- pausa entre registros (em millisegundos) \n" +
     "como parametros.");
@@ -42,11 +43,12 @@ if (!Array.isArray(myArgs) || myArgs.length != 5) {
 p_conn_string = myArgs[0];
 p_usuario = myArgs[1];
 p_senha = myArgs[2];
-p_qt_registros = myArgs[3];
-p_pausa = myArgs[4];
+p_trimestre = myArgs[3];
+p_qt_registros = myArgs[4];
+p_pausa = myArgs[5];
 
 // validacoes
-if (isNaN(parseInt(myArgs[3]))) {
+if (isNaN(parseInt(p_qt_registros))) {
     console.log("ERRO: O parametro p_qt_registros deve ser um numero inteiro");
     return;
 } else {
@@ -58,7 +60,15 @@ if (isNaN(parseInt(myArgs[3]))) {
     }
 }
 
-if (isNaN(parseInt(myArgs[4]))) {
+// Validando o trimestre
+const regexTrimestre = /^\d{4}-[1-4]$/;
+if (!regex.test(p_trimestre)) {
+    console.log("ERRO: O parametro p_trimestre deve estar na mascara YYYY-Q (ex: 2025-1).");
+    return;
+}
+
+// Validando a pausa
+if (isNaN(parseInt(p_pausa))) {
     console.log("ERRO: O parametro p_pausa deve ser um numero inteiro");
     return;
 } else {
@@ -71,8 +81,8 @@ if (isNaN(parseInt(myArgs[4]))) {
 }
 // Fim das validacoes
 
-let qt_regs_num = parseInt(myArgs[3]);
-let pausa_num = parseInt(myArgs[4]);
+let qt_regs_num = parseInt(p_qt_registros);
+let pausa_num = parseInt(p_pausa);
 let contador = 0;
 
 // console.log("\nString de Conexão: \t" + p_conn_string);
@@ -103,7 +113,7 @@ async function run() {
     // Utilizar este estilo de loop for para garantir processamento sincrono.
 
     //console.log(">>> Processando o Aviso: " + aviso);
-    let result = await obtemProcessos(connection, qt_regs_num);
+    let result = await obtemProcessos(connection, qt_regs_num, trimestre);
     //console.log(result);
 
     let row;
@@ -170,7 +180,7 @@ async function solicitaAtualizarPeca(cnj, id_documento) {
     return await getData(cnj);
 }
 
-async function obtemProcessos(connection, numRegistros) {
+async function obtemProcessos(connection, numRegistros, trimestre) {
 
   var result = await connection.execute(
     `
@@ -181,14 +191,24 @@ async function obtemProcessos(connection, numRegistros) {
           tmpp.MTPP_TTDL_DK		          AS TTDL_DK		    ,
           tmpp.MTPP_IN_SIGILO   	      as SIGILO         ,
           tmpp.MTPP_NR_FOLHA_VIRTUAL    as NR_FOLHA_VIRT  ,
-          tmpp.MTPP_DT_DOCUMENTO        as DT_PECA
+          tmpp.MTPP_DT_DOCUMENTO        as DT_PECA        ,
+          to_char(tmpp.MTPP_DT_INCLUSAO, 'YYYY-Q') AS TRIMESTRE_DT_INC 
       FROM TJRJ_METADADOS_PECAS_PROCESSO tmpp 
       WHERE 1=1 
       AND	tmpp.MTPA_DT_EXCLUSAO_PECA IS NULL 
       AND tmpp.MTPP_DT_PESQ_ASSINATURAS IS NULL
-      AND tmpp.MTPP_DT_INCLUSAO >= to_date('01/04/2025', 'dd/mm/yyyy')
-      AND tmpp.MTPP_VT_IN_VERIF_ASSINAT_DIGIT = 'S'
+      AND (
+        MTPP_TTDL_DK NOT IN(185,45,130,145,39,50,9999,53,90) AND 
+        MTPP_NR_PROTOCOLO_PETICAO IS NOT NULL AND 
+        MTPP_CD_IDENTIFICADOR_AVISO IS NULL AND 
+        (MTPP_DT_PESQ_ASSINATURAS IS NULL OR MTPP_DT_PESQ_ASSINATURAS<MTPP_DT_DOWNLOAD+.0833333333333333333333333333333333333333) AND
+        MTPP_DT_DOWNLOAD IS NOT NULL AND
+        MTPA_DT_EXCLUSAO_PECA IS NULL AND
+        LENGTH(TRIM(MTPP_DS_DESCRICAO_DOCUMENTO))>1 AND
+        REGEXP_LIKE (MTPP_DS_DESCRICAO_DOCUMENTO,'[A-Za-z]') 
+		  )
       AND tmpp.MTPP_TTDL_DK not in ( 92, 179, 86, 187 )
+      and to_char(tmpp.MTPP_DT_INCLUSAO, 'YYYY-Q') = :pTrimestre
       AND NOT EXISTS 
       (
         SELECT 1
@@ -197,7 +217,7 @@ async function obtemProcessos(connection, numRegistros) {
       )
       AND ROWNUM <= :pNumRegistros 
  `,
-     {pNumRegistros: numRegistros},  
+     {pNumRegistros: numRegistros, pTrimestre: trimestre},  
      {
         resultSet: true,
         outFormat: oracledb.OUT_FORMAT_OBJECT
